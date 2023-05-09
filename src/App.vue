398c6217c4ef06dce1234a5f946702f24bbab5d2
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-05-09 20:09:56 +0200

    Start of the first TypeScript project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { Job } from '@/types/Job'
+
+const job: Job = {
+  title: 'string',
+  location: 'string',
+  salary: 20,
+  id: 'string'
+}
+</script>
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
